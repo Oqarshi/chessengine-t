a04b6c591cd8da45c54a62cdb5c123eb2466f64c
Oqarshi <94034118+Oqarshi@users.noreply.github.com>
2023-05-28 15:28:36 -0700

    Update garbochess.js

--- js/garbochess.js
+++ js/garbochess.js
@@ -468,7 +468,7 @@ function ScoreMove(move){
     var score;
     if (captured != 0) {
         var pieceType = piece & 0x7;
-        score = (captured << 5) - pieceType;
+        score = Math.abs((captured << 5) - pieceType);
     } else {
         score = historyTable[piece & 0xF][moveTo];
     }
@@ -2473,7 +2473,7 @@ function SeeAddSliderAttacks(target, us, attacks, pieceType) {
 
 function BuildPVMessage(bestMove, value, timeTaken, ply) {
     var totalNodes = g_nodeCount + g_qNodeCount;
-    return "Depth:" + ply + " Eval:" + (Math.abs(value) / 1000).toFixed(1) + " Nodes:" + totalNodes + " NPS:" + ((totalNodes / (timeTaken / 1000)) | 0) + " " + PVFromHash(bestMove, 15);
+    return "Depth:" + ply + " Eval:" + (value / 1000).toFixed(1) + " Nodes:" + totalNodes + " NPS:" + ((totalNodes / (timeTaken / 1000)) | 0) + " " + PVFromHash(bestMove, 15);
 }
 
 //////////////////////////////////////////////////
